f8ba8224384c551aff4ff35772030d4b6432e6bb
alisoncsimons <alisoncsimons@gmail.com>
2021-12-04 11:42:02 -0500

    Stromquist runs

--- stromquist.py
+++ stromquist.py
@@ -10,10 +10,30 @@ class STROMQUIST:
         self.conf = config
         self.agent = StromquistAgent
 
-    def assign_pieces(self):
+    def assign_pieces(self, i, positions, shouter):
         # TODO assign pieces given a stopping point and agent knife positions
         # placeholder return value
-        return [range(self.conf.num_pieces), [], []]
+        allocation = [[],[],[]]
+        sorted_positions = dict(sorted(positions.items(), key=lambda item: item[1]))
+
+        middle_knife = list(sorted_positions.values())[1]
+
+        Left = [x for x in range(i)]
+        Middle = [x for x in range(i, middle_knife)]
+        Right = [x for x in range(middle_knife, self.conf.num_pieces)]
+
+        MiddleTaken = False
+
+        for agent in sorted_positions:
+            if agent == shouter:
+                allocation[agent] = Left
+            elif not MiddleTaken:
+                allocation[agent] = Middle
+                MiddleTaken = True
+            else:
+                allocation[agent] = Right
+
+        return allocation
 
     def run(self, agents):
         if (len(agents)) != 3:
@@ -22,7 +42,11 @@ class STROMQUIST:
         # TODO finish implementing procedure
         # referee sword on the right side of the ith piece
         for i in range(self.conf.num_pieces):
+            positions = {}
             for agent in agents:
-                if agent.shout_cut(i):
-                    return self.assign_pieces()
-        return self.assign_pieces()
+                positions[agent.id] = agent.get_own_knife_position(i)
+            for agent in agents:
+                if agent.shout_cut(i, positions):
+                    shouter = agent.id
+                    return self.assign_pieces(i, positions, shouter)
+        # return self.assign_pieces(i, None, None)
--- stromquist_agent.py
+++ stromquist_agent.py
@@ -9,5 +9,35 @@ class StromquistAgent(Agent):
 
     # TODO implement strategy for stromquist
     # placeholder return value
-    def shout_cut(self, i):
+    def shout_cut(self, i, positions):
+        sorted_pos = list(positions.values())
+        sorted_pos.sort()
+        middle_knife = sorted_pos[1]
+        my_knife = positions[self.id]
+
+        Left = sum(self.V[:i])
+        Middle = sum(self.V[i:middle_knife])
+        Right = sum(self.V[middle_knife:])
+
+        if my_knife >= middle_knife:
+            if Left >= Right:
+                print(f"Agent {self.id} called cut when i is {i}")
+                return True
+        
+        if my_knife < middle_knife:
+            if Left >= Middle:
+                print(f"Agent {self.id} called cut when i is {i}")
+                return True
+        
         return False
+
+    def get_own_knife_position(self, i):
+        cake = self.V[i:]
+        totalvalue = sum(cake)
+        value = 0
+        j = 0
+        while value < totalvalue/2:
+            value += cake[j]
+            j += 1
+        print(f"Agent {self.id} positioned their knife at {i+j}")
+        return i + j
